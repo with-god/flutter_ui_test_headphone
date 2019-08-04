251f900864f6d35c6018f69692742fbd97395501
dev1 <dev1@tonovation.com>
2019-08-04 16:07:41 +0900

    Initial commit

---
+++ lib/model/model.dart
@@ -0,0 +1,14 @@
+class HeadPhone {
+  String image;
+  String title;
+  int color;
+
+  HeadPhone({this.image, this.title, this.color});
+
+}
+
+var products = [
+  HeadPhone(image: "assets/img_01.png", title: "Pampas", color: 6),
+  HeadPhone(image: "assets/img_02.png", title: "Platten TLE", color: 12),
+  HeadPhone(image: "assets/img_03.png", title: "Plattan 2", color: 1),
+];
---
+++ lib/style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final TextStyle searchBarStyle = TextStyle(fontSize: 18, fontFamily: "Montserrat-Medium");
+final TextStyle headingStyle = TextStyle(fontSize: 26, fontFamily: "Montserrat-Bold");
+final TextStyle productTitleStyle = TextStyle(fontSize: 18, fontFamily: "Montserrat-Bold");
+final TextStyle productSubTitleStyle = TextStyle(fontFamily: "Montserrat-SemiBold");
+final TextStyle bottomBarItemStyle = TextStyle(fontSize: 18, fontFamily: "Montserrat-Bold", color: Colors.white);
+final TextStyle speakerTitleStyle = TextStyle(fontSize: 18, fontFamily: "Montserrat-Bold");
+final TextStyle speakerDescStyle = TextStyle(fontSize: 16, color: Colors.grey, fontFamily: "Montserrat-Medium");
